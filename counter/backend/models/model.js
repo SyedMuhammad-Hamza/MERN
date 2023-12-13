b27de9db44a9b6e2506e46e91f4ea54b729fca3f
SyedMuhammad-Hamza <syed1m.hamza@gmail.com>
2023-12-13 06:10:08 -0800

    new changes api

---
+++ counter/backend/models/model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema(
+    {
+        userId:{type:Number,required:true,},
+        title:{type:String,required:true,},
+        body:{type:String,required:true,},
+    },
+    {
+        timestamps:true,
+    }
+)
+export const Book = mongoose.model('Cat',bookSchema);
